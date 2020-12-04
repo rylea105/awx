199398705ba6dbdd58306b48d92c91dcb1fc73ae
nylea <13538834+rylea105@users.noreply.github.com>
2020-12-04 16:46:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 yum install -y epel-release
 
+yum install -y ansible
+
 yum install -y python-virtualenv
 virtualenv venv
 source venv/bin/activate
-
-yum install -y ansible
 
 ansible-playbook ./awx.yml
 
